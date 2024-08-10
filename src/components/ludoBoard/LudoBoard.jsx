84eba8fbbcfa02a5312df36eb8349ae6f3e51d24
vinayBagde <vinaybagde0@gmail.com>
2024-08-10 22:43:47 +0530

    commit

--- src/components/ludoBoard/LudoBoard.jsx
+++ src/components/ludoBoard/LudoBoard.jsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
 export default function LudoBoard() {
+  let [Move, setMove] = useState({blue: 0, yellow: 0, green: 0, red: 0});
   return (
     <>
       <div>
         <p>Game Begins</p>
         <div className="board">
-          <p>Blue moves = </p>
+          <p>Blue moves = {move.blue}</p>
           <button>+1</button>
-          <p>yellow moves = </p>
+          <p>yellow moves = {move.yellow} </p>
           <button>+1</button>
-          <p>Green moves = </p>
+          <p>Green moves = {move.green} </p>
           <button>+1</button>
-          <p>Red moves = </p>
+          <p>Red moves = {move.red} </p>
           <button>+1</button>
         </div>
       </div>
