1cdfe85bb93d5abfbf00403f87ddcfa0b19261a8
vinayBagde <vinaybagde0@gmail.com>
2024-08-10 22:35:08 +0530

    commit

---
+++ src/components/ludoBoard/LudoBoard.jsx
@@ -0,0 +1,19 @@
+export default function LudoBoard() {
+  return (
+    <>
+      <div>
+        <p>Game Begins</p>
+        <div className="board">
+          <p>Blue moves = </p>
+          <button>+1</button>
+          <p>yellow moves = </p>
+          <button>+1</button>
+          <p>Green moves = </p>
+          <button>+1</button>
+          <p>Red moves = </p>
+          <button>+1</button>
+        </div>
+      </div>
+    </>
+  );
+}
